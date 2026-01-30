15a8cb0be2f955ceb0e9c4c328e9668cd4f018e7
wonpyo12 <wonpyo0719@naver.com>
2026-01-30 11:53:51 +0900

    상품선택시 코카콜라가 선택안되는 오류 수정,암바사 선택시 결재가안되는 오류수정

--- 4-5.c
+++ 4-5.c
@@ -4,35 +4,34 @@
 int main() {
 	 
 	char* menu[] = {"코카콜라","밀키스","칠성사이다","웰치스","암바사"};
-	int price[] = { 1000,900,800,1200,700,2000 };;
+	int price[] = { 1000,900,800,1200,700 };;
 	int user_price,number;
 	int i = 0;
 	printf("            자판기             \n");
 	printf("===============================\n");
-	for ( i= 1; i < 6; i++) {
-		printf("%s", menu[i-1]);
+	for (i = 0; i < 5; i++) {
+		printf("%s", menu[i]);
 		printf(": %d ", price[i]);
-		if (i % 3 == 0) {
-			printf("\n");
-		}
+		printf("\n");
+	
 	}
-	printf("\n===============================\n");
+	printf("===============================\n");
 	printf("상품번호를 입력해주세요 : ");
 	scanf("%d", &number);
 	printf("사용하실 금액을 입력해주세요 : ");
 	scanf("%d", &user_price);
 	for (i = 0; i < 6; i++) {
-		if (strcmp(menu[number],menu[i])==0) {
+		if (strcmp(menu[number-1],menu[i])==0) {
 			if (price[i] < user_price) {
 				printf("%d원이 남습니다. 거스름돈 반환\n", user_price - price[i]);
-				printf("결재완료!! 상품 %s가 %d원결재되었습니다.상품을 가져가 주세요", menu[i - 1], price[i]);
+				printf("결재완료!! 상품 %s가 %d원결재되었습니다.상품을 가져가 주세요", menu[i], price[i]);
 			}
 			else if (price[i] > user_price) {
 				printf("%d원이 부족합니다.\n", price[i] - user_price);
 				printf("%d원을 반환합니다. 다음에 다시 이용해주세요", user_price);
 			}
 			else if(price[i] == user_price) {
-				printf("결재완료!! 상품 %s가 %d원결재되었습니다.상품을 가져가 주세요", menu[i-1], price[i]);
+				printf("결재완료!! 상품 %s가 %d원결재되었습니다.상품을 가져가 주세요", menu[i], price[i]);
 			}
 			break;
 		}
