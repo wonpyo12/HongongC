9c2f6225af8f219be0d1cfce555af7053945ea34
wonpyo12 <wonpyo0719@naver.com>
2026-01-30 12:42:57 +0900

    금액 부족시 추가금액 입력

--- 4-5.c
+++ 4-5.c
@@ -5,8 +5,9 @@ int main() {
 	 
 	char* menu[] = {"코카콜라","밀키스","칠성사이다","웰치스","암바사"};
 	int price[] = { 1000,900,800,1200,700 };;
-	int user_price,number;
+	int user_price,number,priceadd;
 	int i = 0;
+	char a[10];
 	printf("            자판기             \n");
 	printf("===============================\n");
 	for (i = 0; i < 5; i++) {
@@ -23,7 +24,7 @@ int main() {
 	printf("잔액 : %d\n", user_price);
 	for (i = 0; i < 6; i++) {
 		if (number<1 || number>6) {
-			printf("없는 상품 번호입니다.\n");
+			printf("없는 상품 번호입니다 : ");
 			printf("상품번호를 다시입력해주세요 : ");
 			scanf("%d", &number);
 		}
@@ -33,14 +34,25 @@ int main() {
 				printf("결재완료!! 상품 %s가 %d원결재되었습니다.상품을 가져가 주세요", menu[i], price[i]);
 			}
 			else if (price[i] > user_price) {
-				printf("%d원이 부족합니다.\n", price[i] - user_price);
+				printf("%d원이 부족합니다.금액을 추가로 넣으시겠습니까?(yes/no) : ", price[i] - user_price);
+				scanf("%s", &a);
 				
-				printf("%d원을 반환합니다. 다음에 다시 이용해주세요", user_price);
+				if (strcmp(a,"yes")==0) {
+					printf("추가로 넣으실 금액을 입력해주세요 : ");
+					scanf("%d", &priceadd);
+					user_price = user_price+priceadd;
+					i = 0;
+				}
+				else if (strcmp(a,"no")==0) {
+					printf("%d원을 반환합니다. 다음에 다시 이용해주세요", user_price);
+					break;
+				}
+				
 			}
 			else if(price[i] == user_price) {
 				printf("결재완료!! 상품 %s가 %d원결재되었습니다.상품을 가져가 주세요", menu[i], price[i]);
 			}
-			break;
+			
 		}
 		
 	}
